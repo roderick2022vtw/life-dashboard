98311c77ed6359f7a345e28b6ec902edc6c5ef64
roderick2022vtw <roderick@vantwout.net>
2026-06-09 18:02:22 +0200

    Add PWA support: manifest, service worker, iOS meta tags

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'life-os-v1';
+const ASSETS = [
+  '/life-dashboard/',
+  '/life-dashboard/index.html',
+  '/life-dashboard/manifest.json',
+  '/life-dashboard/icon.svg',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
